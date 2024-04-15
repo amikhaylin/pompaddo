0d1b4569b331f5ddd2415fc93f605777331e59fa
Andrey Mikhaylin <cenobith@gmail.com>
2024-04-15 19:25:23 +0300

    Views for projects review

---
+++ VeraPlayaMac/VeraPlayaMac/Activities/Projects/Review/ReviewProjectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewProjectsView.swift
+//  VeraPlayaMac
+//
+//  Created by Andrey Mikhaylin on 15.04.2024.
+//
+
+import SwiftUI
+
+struct ReviewProjectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewProjectsView()
+}
