e01d8243f7df9abefd6b927d6f02c0fa1c1b7abb
Andrey Mikhaylin <cenobith@gmail.com>
2024-05-24 15:43:32 +0300

    Filter completed in inbox badge

--- PomPadDoMac/PomPadDoMac/Activities/ContentView.swift
+++ PomPadDoMac/PomPadDoMac/Activities/ContentView.swift
@@ -66,7 +66,7 @@ struct ContentView: View {
                                     Text("Inbox")
                                 }
                                 .foregroundStyle(Color(#colorLiteral(red: 0.4890732765, green: 0.530819118, blue: 0.7039532065, alpha: 1)))
-                                .badge(tasksInbox.count)
+                                .badge(tasksInbox.filter({ $0.completed == false}).count)
                             }
                             .dropDestination(for: Todo.self) { tasks, _ in
                                 for task in tasks {
