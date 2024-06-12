ed92e5edc2abf7bc8d0cf8dcfe61891f2dd00f8f
Andrey Mikhaylin <cenobith@gmail.com>
2024-06-12 19:14:49 +0300

    Extracted Section List view

--- PomPadDoiOS/PomPadDoiOS/Activities/ContentView.swift
+++ PomPadDoiOS/PomPadDoiOS/Activities/ContentView.swift
@@ -43,11 +43,11 @@ struct ContentView: View {
     
     @State private var selectedProject: Project?
     
+    @Query var projects: [Project]
+    
     @Query(filter: TasksQuery.predicateInbox()) var tasksInbox: [Todo]
     @Query(filter: TasksQuery.predicateToday()) var tasksToday: [Todo]
     @Query(filter: TasksQuery.predicateTomorrow()) var tasksTomorrow: [Todo]
-    
-    @Query var projects: [Project]
     
     @Query(filter: TasksQuery.predicateTodayActive()) var tasksTodayActive: [Todo]
     @State var badgeManager = BadgeManager()
@@ -55,90 +55,8 @@ struct ContentView: View {
     var body: some View {
         NavigationSplitView {
             VStack {
-                List(SideBarItem.allCases, selection: $selectedSideBarItem) { item in
-                    switch item {
-                    case .inbox:
-                        NavigationLink(value: item) {
-                            HStack {
-                                Image(systemName: "tray")
-                                Text("Inbox")
-                            }
-                            .foregroundStyle(Color(#colorLiteral(red: 0.4890732765, green: 0.530819118, blue: 0.7039532065, alpha: 1)))
-                            .badge(tasksInbox.filter({ $0.completed == false}).count)
-                        }
-                        .dropDestination(for: Todo.self) { tasks, _ in
-                            for task in tasks {
-                                if let project = task.project, let index = project.tasks?.firstIndex(of: task) {
-                                    task.project?.tasks?.remove(at: index)
-                                    task.project = nil
-                                    task.status = nil
-                                }
-                                if let parentTask = task.parentTask,
-                                   let index = parentTask.subtasks?.firstIndex(of: task) {
-                                    task.parentTask = nil
-                                    parentTask.subtasks?.remove(at: index)
-                                }
-                            }
-                            return true
-                        }
-                        
-                    case .today:
-                        NavigationLink(value: item) {
-                            HStack {
-                                Image(systemName: "calendar")
-                                Text("Today")
-                            }
-                            .foregroundStyle(Color(#colorLiteral(red: 0.9496305585, green: 0.5398437977, blue: 0.3298020959, alpha: 1)))
-                            .badge(tasksToday.filter({ $0.completed == false}).count)
-                        }
-                        .dropDestination(for: Todo.self) { tasks, _ in
-                            for task in tasks {
-                                task.dueDate = Calendar.current.startOfDay(for: Date())
-                            }
-                            return true
-                        }
-                    case .tomorrow:
-                        NavigationLink(value: item) {
-                            HStack {
-                                Image(systemName: "sunrise")
-                                Text("Tomorrow")
-                            }
-                            .foregroundStyle(Color(#colorLiteral(red: 0.9219498038, green: 0.2769843042, blue: 0.402439177, alpha: 1)))
-                            .badge(tasksTomorrow.filter({ $0.completed == false }).count)
-                        }
-                        .dropDestination(for: Todo.self) { tasks, _ in
-                            for task in tasks {
-                                task.dueDate = Calendar.current.date(byAdding: .day, value: 1, to: Calendar.current.startOfDay(for: Date()))
-                            }
-                            return true
-                        }
-                    case .projects:
-                        EmptyView()
-                    case .review:
-                        NavigationLink(value: item) {
-                            HStack {
-                                Image(systemName: "cup.and.saucer")
-                                Text("Review")
-                            }
-                            .foregroundStyle(Color(#colorLiteral(red: 0.480404973, green: 0.507386148, blue: 0.9092046022, alpha: 1)))
-                            .badge(projects.filter({
-                                if $0.showInReview == false {
-                                    return false
-                                }
-                                let today = Date()
-                                if let dateToReview = Calendar.current.date(byAdding: .day,
-                                                                            value: $0.reviewDaysCount,
-                                                                            to: $0.reviewDate) {
-                                    return dateToReview <= today
-                                } else {
-                                    return false
-                                }
-                            }).count)
-                        }
-                    }
-                }
-                .listStyle(SidebarListStyle())
-                .frame(height: 220)
+                SectionsListView(selectedSideBarItem: $selectedSideBarItem)
+                    .frame(height: 220)
                 
                 ProjectsListView(selectedProject: $selectedProject,
                                  projects: projects)
---
+++ PomPadDoiOS/PomPadDoiOS/Activities/Tasks/MainTasksListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTasksListView.swift
+//  PomPadDoiOS
+//
+//  Created by Andrey Mikhaylin on 12.06.2024.
+//
+
+import SwiftUI
+
+struct MainTasksListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTasksListView()
+}
