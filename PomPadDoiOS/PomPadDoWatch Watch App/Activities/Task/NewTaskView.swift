f206d1b988d60b3ab40707cb24bde28a593d0d86
Andrey Mikhaylin <cenobith@gmail.com>
2024-06-25 12:39:19 +0300

    Moved to companion app

---
+++ PomPadDoiOS/PomPadDoWatch Watch App/Activities/Task/NewTaskView.swift
@@ -0,0 +1,34 @@
+//
+//  NewTaskView.swift
+//  PomPadDoWatch Watch App
+//
+//  Created by Andrey Mikhaylin on 25.06.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct NewTaskView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.presentationMode) var presentationMode
+    @State private var taskName = ""
+    
+    var body: some View {
+        NavigationView {
+            TextField("Add task to Inbox", text: $taskName)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("OK") {
+                            let task = Todo(name: taskName)
+                            modelContext.insert(task)
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    NewTaskView()
+}
---
+++ PomPadDoiOS/PomPadDoWatch Watch App/Activities/Task/TasksListView.swift
@@ -0,0 +1,45 @@
+//
+//  TasksListView.swift
+//  PomPadDoWatch Watch App
+//
+//  Created by Andrey Mikhaylin on 25.06.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct TasksListView: View {
+    @Environment(\.modelContext) private var modelContext
+    @State var tasks: [Todo]
+    
+    @State var list: SideBarItem
+    @State var title: String
+    @State var mainTask: Todo?
+    
+    var body: some View {
+        NavigationStack {
+            List(tasks) { task in
+                HStack {
+                    TaskCheckBoxView(task: task)
+                    
+                    Text(task.name)
+                }
+            }
+        }
+        .navigationTitle(title)
+    }
+}
+
+#Preview {
+    do {
+        let previewer = try Previewer()
+        let tasks: [Todo] = [previewer.task]
+        
+        return TasksListView(tasks: tasks,
+                             list: .inbox,
+                             title: "Some list")
+            .modelContainer(previewer.container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
+}
