38a930dc4c9214f9ad3a45110fb2a65d157cd9ca
Andrey Mikhaylin <cenobith@gmail.com>
2024-09-11 10:38:58 +0300

    Tests timer window

--- PomPadDoMac/PomPadDoMacUITests/PomPadDoMacUITests.swift
+++ PomPadDoMac/PomPadDoMacUITests/PomPadDoMacUITests.swift
@@ -9,12 +9,17 @@
 import XCTest
 
 final class PomPadDoMacUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -23,12 +28,8 @@ final class PomPadDoMacUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    // TODO: Make UI tests
-    func testMainWindow() throws {
+    func testAMainWindow() throws {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         let moveButton = app/*@START_MENU_TOKEN@*/.outlines.matching(identifier: "Sidebar").buttons["Move"]/*[[".splitGroups[\"SwiftUI.ModifiedContent<PomPadDo.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<PomPadDo.Refresher>>>-1-AppWindow-1, SidebarNavigationSplitView\"]",".groups",".scrollViews.outlines.matching(identifier: \"Sidebar\")",".outlineRows",".cells.buttons[\"Move\"]",".buttons[\"Move\"]",".outlines.matching(identifier: \"Sidebar\")"],[[[-1,6,3],[-1,2,3],[-1,1,2],[-1,0,1]],[[-1,6,3],[-1,2,3],[-1,1,2]],[[-1,6,3],[-1,2,3]],[[-1,5],[-1,4],[-1,3,4]],[[-1,5],[-1,4]]],[0,0]]@END_MENU_TOKEN@*/
@@ -115,16 +116,9 @@ final class PomPadDoMacUITests: XCTestCase {
             XCTFail("Delay interrupted")
         }
         
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Filled projects and tasks"
-        attachment.lifetime = .keepAlways
-        add(attachment)
     }
     
-    func testAddSubtasks() throws {
-        let app = XCUIApplication()
-        app.launch()
-        
+    func testBAddSubtasks() throws {
         app.outlines.cells.containing(.staticText, identifier: "Design data model").element.rightClick()
         app/*@START_MENU_TOKEN@*/.outlines.menuItems["Add subtask"]/*[[".splitGroups[\"SwiftUI.ModifiedContent<PomPadDo.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<PomPadDo.Refresher>>>-1-AppWindow-1, SidebarNavigationSplitView\"]",".splitGroups",".groups",".scrollViews.outlines",".menus.menuItems[\"Add subtask\"]",".menuItems[\"Add subtask\"]",".outlines"],[[[-1,6,4],[-1,3,4],[-1,2,3],[-1,1,2],[-1,0,1]],[[-1,6,4],[-1,3,4],[-1,2,3],[-1,1,2]],[[-1,6,4],[-1,3,4],[-1,2,3]],[[-1,6,4],[-1,3,4]],[[-1,5],[-1,4]]],[0,0]]@END_MENU_TOKEN@*/.click()
         app/*@START_MENU_TOKEN@*/.groups/*[[".splitGroups[\"SwiftUI.ModifiedContent<PomPadDo.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<PomPadDo.Refresher>>>-1-AppWindow-1, SidebarNavigationSplitView\"]",".splitGroups",".scrollViews.groups",".groups"],[[[-1,3],[-1,2],[-1,1,2],[-1,0,1]],[[-1,3],[-1,2],[-1,1,2]],[[-1,3],[-1,2]]],[0]]@END_MENU_TOKEN@*/.textFields["EditTaskName"].click()
@@ -146,6 +140,30 @@ final class PomPadDoMacUITests: XCTestCase {
             attachment.name = "Added subtasks"
             attachment.lifetime = .keepAlways
             add(attachment)
+        } else {
+            XCTFail("Delay interrupted")
+        }
+    }
+    
+    func testCTimerWindow() throws {
+        app.children(matching: .menuBar).element(boundBy: 1).children(matching: .statusItem).element.click()
+        
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Timer window tasks"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+        
+        app.groups.containing(.textField, identifier: "Add task to Inbox").outlines.cells.containing(.staticText, identifier: "Design data model").buttons["Play"].click()
+        
+        let exp = expectation(description: "Screenshot after 10 seconds")
+        let result = XCTWaiter.wait(for: [exp], timeout: 10.0)
+        if result == XCTWaiter.Result.timedOut {
+            let attachment = XCTAttachment(screenshot: app.screenshot())
+            attachment.name = "Timer window"
+            attachment.lifetime = .keepAlways
+            add(attachment)
+            
+            app/*@START_MENU_TOKEN@*/.buttons["Stop"]/*[[".dialogs",".groups.buttons[\"Stop\"]",".buttons[\"Stop\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.click()
         } else {
             XCTFail("Delay interrupted")
         }
