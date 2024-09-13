58a64798ff0d0ab99068e44cd3116db7d76dd686
Andrey Mikhaylin <cenobith@gmail.com>
2024-09-13 12:22:31 +0300

    Connected fastline snapshot

--- PomPadDoiOS/PomPadDoiOSUITests/PomPadDoiOSUITests.swift
+++ PomPadDoiOS/PomPadDoiOSUITests/PomPadDoiOSUITests.swift
@@ -8,13 +8,32 @@
 import XCTest
 
 final class PomPadDoiOSUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
-    override func setUpWithError() throws {
+    @MainActor override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
 
+        app = XCUIApplication()
+
+        if ProcessInfo.processInfo.environment["IS_FASTLANE"] == "YES" {
+            setupSnapshot(app)
+        }
+        
+        app.launch()
+
+        addUIInterruptionMonitor(withDescription: "Tracking Usage Permission Alert") { (alert) -> Bool in
+                if alert.buttons["Allow"].exists {
+                    alert.buttons["Allow"].tap()
+                    self.app.activate()
+                    return true
+                }
+                return false
+            }
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -24,8 +43,6 @@ final class PomPadDoiOSUITests: XCTestCase {
 
     func testExample() throws {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
