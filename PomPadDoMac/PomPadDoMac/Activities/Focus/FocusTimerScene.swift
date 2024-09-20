303c0b739d766f73f47968c21fa0e9c22cc372f4
Andrey Mikhaylin <cenobith@gmail.com>
2024-09-20 16:15:29 +0300

    Timer menu color

--- PomPadDoMac/PomPadDoMac/Activities/Focus/FocusTimerScene.swift
+++ PomPadDoMac/PomPadDoMac/Activities/Focus/FocusTimerScene.swift
@@ -12,6 +12,7 @@ struct FocusTimerScene: Scene {
     @EnvironmentObject var refresher: Refresher
     @State private var timerCount: String = ""
     @State private var focusMode: FocusTimerMode = .work
+    @State private var focusState: FocusTimerState = .idle
     
     @StateObject var timer = FocusTimer(workInSeconds: 1500,
                            breakInSeconds: 300,
@@ -27,28 +28,37 @@ struct FocusTimerScene: Scene {
             .environmentObject(timer)
             .modelContext(modelContext)
         } label: {
-            HStack {
-                if focusMode == .work {
-                    let configuration = NSImage.SymbolConfiguration(pointSize: 16, weight: .light)
-//                        .applying(.init(hierarchicalColor: .yellow))
-                        .applying(.init(hierarchicalColor: .red))
+            if focusState == .idle {
+                let configuration = NSImage.SymbolConfiguration(pointSize: 16, weight: .light)
+                
+                let image = NSImage(systemSymbolName: "target", accessibilityDescription: nil)
+                let updateImage = image?.withSymbolConfiguration(configuration)
+                
+                Image(nsImage: updateImage!)
+            } else {
+                HStack {
+                    if focusMode == .work {
+                        let configuration = NSImage.SymbolConfiguration(pointSize: 16, weight: .light)
+                        //                        .applying(.init(hierarchicalColor: .yellow))
+                            .applying(.init(hierarchicalColor: .red))
+                        
+                        let image = NSImage(systemSymbolName: "target", accessibilityDescription: nil)
+                        let updateImage = image?.withSymbolConfiguration(configuration)
+                        
+                        Image(nsImage: updateImage!)
+                    } else {
+                        let configuration = NSImage.SymbolConfiguration(pointSize: 16, weight: .light)
+                        //                        .applying(.init(hierarchicalColor: .blue))
+                            .applying(.init(hierarchicalColor: .green))
+                        
+                        let image = NSImage(systemSymbolName: "cup.and.saucer.fill", accessibilityDescription: nil)
+                        let updateImage = image?.withSymbolConfiguration(configuration)
+                        
+                        Image(nsImage: updateImage!) // This works.
+                    }
                     
-                    let image = NSImage(systemSymbolName: "target", accessibilityDescription: nil)
-                    let updateImage = image?.withSymbolConfiguration(configuration)
-                    
-                    Image(nsImage: updateImage!)
-                } else {
-                    let configuration = NSImage.SymbolConfiguration(pointSize: 16, weight: .light)
-//                        .applying(.init(hierarchicalColor: .blue))
-                        .applying(.init(hierarchicalColor: .green))
-                                    
-                    let image = NSImage(systemSymbolName: "cup.and.saucer.fill", accessibilityDescription: nil)
-                    let updateImage = image?.withSymbolConfiguration(configuration)
-
-                    Image(nsImage: updateImage!) // This works.
+                    Text(timerCount)
                 }
-
-                Text(timerCount)
             }
         }
         .menuBarExtraStyle(.window)
@@ -58,5 +68,8 @@ struct FocusTimerScene: Scene {
         .onChange(of: timer.mode, { _, _ in
             focusMode = timer.mode
         })
+        .onChange(of: timer.state) { _, _ in
+            focusState = timer.state
+        }
     }
 }
