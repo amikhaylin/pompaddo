140ac835470b1e5eb0644d2bb6b04819032a9394
Andrey Mikhaylin <cenobith@gmail.com>
2026-01-27 18:24:23 +0300

    review period

--- PomPadDo.mobile/MainView.swift
+++ PomPadDo.mobile/MainView.swift
@@ -47,154 +47,152 @@ struct MainView: View {
     @State var selectedProject: Project?
     
     var body: some View {
-//        GeometryReader { geometry in
-            VStack {
-                Group {
-                    switch tab {
-                    case .tasks:
-                        ContentView(selectedSideBarItem: $selectedSideBarItem,
-                                    selectedProject: $selectedProject)
-                            .id(refresher.refresh)
-                            .environmentObject(refresher)
-                            .environmentObject(timer)
-                            .environmentObject(focusTask)
-                    case .focus:
-                        FocusTimerView(focusMode: $focusMode)
-                            .id(refresh)
-                            .environmentObject(timer)
-                            .environmentObject(focusTask)
-                            .refreshable {
-                                refresh.toggle()
-                            }
-                    case.settings:
-                        SettingsView()
-                    }
-                }
-                
-                HStack {
-                    Spacer()
-                    
-                    Button {
-                        tab = .tasks
-                    } label: {
-                        Image(systemName: "checkmark.square")
-                    }
-                    .modifier(ConditionalButtonStyle(condition: tab == .tasks))
-                    .buttonBorderShape(.capsule)
-                    .accessibility(identifier: "TasksSection")
-                    
-                    Spacer()
-                    
-                    Button {
-                        tab = .focus
-                    } label: {
-                        FocusTabItemView()
-                            .environmentObject(timer)
-                    }
-                    .modifier(ConditionalButtonStyle(condition: tab == .focus))
-                    .buttonBorderShape(.capsule)
-                    .accessibility(identifier: "FocusSection")
-                    
-                    Spacer()
-                    
-                    Button {
-                        tab = .settings
-                    } label: {
-                        Image(systemName: "gear")
-                    }
-                    .modifier(ConditionalButtonStyle(condition: tab == .settings))
-                    .buttonBorderShape(.capsule)
-                    .accessibility(identifier: "SettingsSection")
-                    
-                    Spacer()
-                    
-                    Button {
-                        newTaskIsShowing.toggle()
-                    } label: {
-                        Image(systemName: "tray.and.arrow.down.fill")
-                            .foregroundStyle(Color.orange)
-                    }
-                    .accessibility(identifier: "AddTaskToInboxButton")
-                    .keyboardShortcut("i", modifiers: [.command])
-                    .sheet(isPresented: $newTaskIsShowing, content: {
-                        NewTaskView(isVisible: self.$newTaskIsShowing, list: .inbox, project: nil, mainTask: nil)
-                            .presentationDetents([.height(220)])
-                            .presentationDragIndicator(.visible)
-                            .environmentObject(refresher)
-                    })
-                    .modifier(GlassButtonStyle())
-                    .buttonBorderShape(.capsule)
-                    
-                    Spacer()
-                }
-            }
-            .onAppear {
-                timer.setDurations(workInSeconds: timerWorkSession,
-                                   breakInSeconds: timerBreakSession,
-                                   longBreakInSeconds: timerLongBreakSession,
-                                   workSessionsCount: Int(timerWorkSessionsCount))
-                
-                checkForReview()
-            }
-            .onChange(of: timerWorkSession, { _, _ in
-                timer.setDurations(workInSeconds: timerWorkSession,
-                                   breakInSeconds: timerBreakSession,
-                                   longBreakInSeconds: timerLongBreakSession,
-                                   workSessionsCount: Int(timerWorkSessionsCount))
-            })
-            .onChange(of: timerBreakSession, { _, _ in
-                timer.setDurations(workInSeconds: timerWorkSession,
-                                   breakInSeconds: timerBreakSession,
-                                   longBreakInSeconds: timerLongBreakSession,
-                                   workSessionsCount: Int(timerWorkSessionsCount))
-            })
-            .onChange(of: timerLongBreakSession, { _, _ in
-                timer.setDurations(workInSeconds: timerWorkSession,
-                                   breakInSeconds: timerBreakSession,
-                                   longBreakInSeconds: timerLongBreakSession,
-                                   workSessionsCount: Int(timerWorkSessionsCount))
-            })
-            .onChange(of: timerWorkSessionsCount, { _, _ in
-                timer.setDurations(workInSeconds: timerWorkSession,
-                                   breakInSeconds: timerBreakSession,
-                                   longBreakInSeconds: timerLongBreakSession,
-                                   workSessionsCount: Int(timerWorkSessionsCount))
-            })
-            .onChange(of: timer.mode, { _, _ in
-                focusMode = timer.mode
-            })
-            .onChange(of: timer.sessionsCounter, { _, newValue in
-                if let task = focusTask.task, newValue > 0 {
-                    task.tomatoesCount += 1
-                }
-            })
-            .onOpenURL { url in
-                if url.scheme == "pompaddo" && url.host == "addtoinbox" {
-                    newTaskIsShowing.toggle()
-                } else if url.scheme == "pompaddo" && url.host == "new" {
-                    print(url.absoluteString)
-                    let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
-                    if let title = components?.queryItems?.first(where: { $0.name == "title" })?.value {
-                        let task = Todo(name: title)
-                        if let link = components?.queryItems?.first(where: { $0.name == "link" })?.value, let linkurl = URL(string: link) {
-                            task.link = linkurl.absoluteString
+        VStack {
+            Group {
+                switch tab {
+                case .tasks:
+                    ContentView(selectedSideBarItem: $selectedSideBarItem,
+                                selectedProject: $selectedProject)
+                        .id(refresher.refresh)
+                        .environmentObject(refresher)
+                        .environmentObject(timer)
+                        .environmentObject(focusTask)
+                case .focus:
+                    FocusTimerView(focusMode: $focusMode)
+                        .id(refresh)
+                        .environmentObject(timer)
+                        .environmentObject(focusTask)
+                        .refreshable {
+                            refresh.toggle()
                         }
-                        modelContext.insert(task)
+                case.settings:
+                    SettingsView()
+                }
+            }
+            
+            HStack {
+                Spacer()
+                
+                Button {
+                    tab = .tasks
+                } label: {
+                    Image(systemName: "checkmark.square")
+                }
+                .modifier(ConditionalButtonStyle(condition: tab == .tasks))
+                .buttonBorderShape(.capsule)
+                .accessibility(identifier: "TasksSection")
+                
+                Spacer()
+                
+                Button {
+                    tab = .focus
+                } label: {
+                    FocusTabItemView()
+                        .environmentObject(timer)
+                }
+                .modifier(ConditionalButtonStyle(condition: tab == .focus))
+                .buttonBorderShape(.capsule)
+                .accessibility(identifier: "FocusSection")
+                
+                Spacer()
+                
+                Button {
+                    tab = .settings
+                } label: {
+                    Image(systemName: "gear")
+                }
+                .modifier(ConditionalButtonStyle(condition: tab == .settings))
+                .buttonBorderShape(.capsule)
+                .accessibility(identifier: "SettingsSection")
+                
+                Spacer()
+                
+                Button {
+                    newTaskIsShowing.toggle()
+                } label: {
+                    Image(systemName: "tray.and.arrow.down.fill")
+                        .foregroundStyle(Color.orange)
+                }
+                .accessibility(identifier: "AddTaskToInboxButton")
+                .keyboardShortcut("i", modifiers: [.command])
+                .sheet(isPresented: $newTaskIsShowing, content: {
+                    NewTaskView(isVisible: self.$newTaskIsShowing, list: .inbox, project: nil, mainTask: nil)
+                        .presentationDetents([.height(220)])
+                        .presentationDragIndicator(.visible)
+                        .environmentObject(refresher)
+                })
+                .modifier(GlassButtonStyle())
+                .buttonBorderShape(.capsule)
+                
+                Spacer()
+            }
+        }
+        .onAppear {
+            timer.setDurations(workInSeconds: timerWorkSession,
+                               breakInSeconds: timerBreakSession,
+                               longBreakInSeconds: timerLongBreakSession,
+                               workSessionsCount: Int(timerWorkSessionsCount))
+            
+            checkForReview()
+        }
+        .onChange(of: timerWorkSession, { _, _ in
+            timer.setDurations(workInSeconds: timerWorkSession,
+                               breakInSeconds: timerBreakSession,
+                               longBreakInSeconds: timerLongBreakSession,
+                               workSessionsCount: Int(timerWorkSessionsCount))
+        })
+        .onChange(of: timerBreakSession, { _, _ in
+            timer.setDurations(workInSeconds: timerWorkSession,
+                               breakInSeconds: timerBreakSession,
+                               longBreakInSeconds: timerLongBreakSession,
+                               workSessionsCount: Int(timerWorkSessionsCount))
+        })
+        .onChange(of: timerLongBreakSession, { _, _ in
+            timer.setDurations(workInSeconds: timerWorkSession,
+                               breakInSeconds: timerBreakSession,
+                               longBreakInSeconds: timerLongBreakSession,
+                               workSessionsCount: Int(timerWorkSessionsCount))
+        })
+        .onChange(of: timerWorkSessionsCount, { _, _ in
+            timer.setDurations(workInSeconds: timerWorkSession,
+                               breakInSeconds: timerBreakSession,
+                               longBreakInSeconds: timerLongBreakSession,
+                               workSessionsCount: Int(timerWorkSessionsCount))
+        })
+        .onChange(of: timer.mode, { _, _ in
+            focusMode = timer.mode
+        })
+        .onChange(of: timer.sessionsCounter, { _, newValue in
+            if let task = focusTask.task, newValue > 0 {
+                task.tomatoesCount += 1
+            }
+        })
+        .onOpenURL { url in
+            if url.scheme == "pompaddo" && url.host == "addtoinbox" {
+                newTaskIsShowing.toggle()
+            } else if url.scheme == "pompaddo" && url.host == "new" {
+                print(url.absoluteString)
+                let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+                if let title = components?.queryItems?.first(where: { $0.name == "title" })?.value {
+                    let task = Todo(name: title)
+                    if let link = components?.queryItems?.first(where: { $0.name == "link" })?.value, let linkurl = URL(string: link) {
+                        task.link = linkurl.absoluteString
                     }
-                } else {
-                    return
+                    modelContext.insert(task)
                 }
+            } else {
+                return
             }
-            .onChange(of: scenePhase) { _, newPhase in
-                if newPhase == .background && timer.state == .running {
-                    timer.setNotification()
-                }
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            if newPhase == .background && timer.state == .running {
+                timer.setNotification()
             }
-//        }
+        }
     }
     
     private func checkForReview() {
-        let daysBeforeRequest = 7
+        let daysBeforeRequest = 3
         let currentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
         
         print("currentVersion: \(currentVersion)")
