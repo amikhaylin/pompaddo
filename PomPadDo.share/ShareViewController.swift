804c6c74272aa00d401b9d086456bc0f383e0fb1
Andrey Mikhaylin <cenobith@gmail.com>
2026-02-04 18:01:07 +0300

    Created share extension for desktop

---
+++ PomPadDo.share/ShareViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ShareViewController.swift
+//  PomPadDo.share
+//
+//  Created by Andrey Mikhaylin on 04.02.2026.
+//
+
+import Cocoa
+
+class ShareViewController: NSViewController {
+
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("ShareViewController")
+    }
+
+    override func loadView() {
+        super.loadView()
+    
+        // Insert code here to customize the view
+        guard let item = self.extensionContext?.inputItems.first as? NSExtensionItem else {
+            NSLog("First input item is not an NSExtensionItem or inputItems is missing")
+            return
+        }
+        if let attachments = item.attachments {
+            NSLog("Attachments = %@", attachments as NSArray)
+        } else {
+            NSLog("No Attachments")
+        }
+    }
+
+    @IBAction func send(_ sender: AnyObject?) {
+        let outputItem = NSExtensionItem()
+        // Complete implementation by setting the appropriate value on the output item
+    
+        let outputItems = [outputItem]
+        self.extensionContext!.completeRequest(returningItems: outputItems, completionHandler: nil)
+    }
+
+    @IBAction func cancel(_ sender: AnyObject?) {
+        let cancelError = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
+        self.extensionContext!.cancelRequest(withError: cancelError)
+    }
+
+}
