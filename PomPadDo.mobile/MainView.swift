72f2e75562b68c50b7832dd6147de050eaf95e4b
Andrey Mikhaylin <cenobith@gmail.com>
2026-02-26 21:21:32 +0300

    Clean code

--- PomPadDo.mobile/MainView.swift
+++ PomPadDo.mobile/MainView.swift
@@ -47,58 +47,53 @@ struct MainView: View {
     @State var activeTasksCount: Int = 0
     
     var body: some View {
-        ZStack {
-            TabView(selection: $tab) {
-                Tab(value: .tasks) {
-                    ContentView(selectedSideBarItem: $selectedSideBarItem,
-                                selectedProject: $selectedProject,
-                                activeTasksCount: $activeTasksCount)
-                    .id(refresher.refresh)
-                    .environmentObject(refresher)
+        TabView(selection: $tab) {
+            Tab(value: .tasks) {
+                ContentView(selectedSideBarItem: $selectedSideBarItem,
+                            selectedProject: $selectedProject,
+                            activeTasksCount: $activeTasksCount)
+                .id(refresher.refresh)
+                .environmentObject(refresher)
+                .environmentObject(timer)
+                .environmentObject(focusTask)
+            } label: {
+                Label("Tasks", systemImage: "checkmark.square")
+                    .accessibility(identifier: "TasksSection")
+            }
+            .badge(activeTasksCount)
+            
+            Tab(value: .focus) {
+                FocusTimerView(focusMode: $focusMode)
+                    .id(refresh)
                     .environmentObject(timer)
                     .environmentObject(focusTask)
-                    .tag(0)
-                } label: {
-                    Label("Tasks", systemImage: "checkmark.square")
-                        .accessibility(identifier: "TasksSection")
-                }
-                .badge(activeTasksCount)
-                
-                Tab(value: .focus) {
-                    FocusTimerView(focusMode: $focusMode)
-                        .id(refresh)
-                        .environmentObject(timer)
-                        .environmentObject(focusTask)
-                        .refreshable {
-                            refresh.toggle()
-                        }
-                        .tag(1)
-                } label: {
-                    FocusTabItemView()
-                        .environmentObject(timer)
-                        .accessibility(identifier: "FocusSection")
-                }
-                .badge(timer.secondsLeftString)
-                
-                Tab(value: .settings) {
-                    SettingsView()
-                        .tag(2)
-                } label: {
-                    Label("Settings", systemImage: "gear")
-                        .accessibility(identifier: "SettingsSection")
-                }
-                
-                Tab(value: .inbox, role: .search) {
-                    EmptyView()
-                } label: {
-                    Label("Add to Inbox", systemImage: "tray.and.arrow.down.fill")
-                        .foregroundStyle(Color.orange)
-                        .accessibility(identifier: "AddTaskToInboxButton")
-                        .keyboardShortcut("i", modifiers: [.command])
-                }
+                    .refreshable {
+                        refresh.toggle()
+                    }
+            } label: {
+                FocusTabItemView()
+                    .environmentObject(timer)
+                    .accessibility(identifier: "FocusSection")
             }
-            .tabViewStyle(.tabBarOnly)
+            .badge(timer.secondsLeftString)
+            
+            Tab(value: .settings) {
+                SettingsView()
+            } label: {
+                Label("Settings", systemImage: "gear")
+                    .accessibility(identifier: "SettingsSection")
+            }
+            
+            Tab(value: .inbox, role: .search) {
+                EmptyView()
+            } label: {
+                Label("Add to Inbox", systemImage: "tray.and.arrow.down.fill")
+                    .foregroundStyle(Color.orange)
+                    .accessibility(identifier: "AddTaskToInboxButton")
+                    .keyboardShortcut("i", modifiers: [.command])
+            }
         }
+        .tabViewStyle(.tabBarOnly)
         .sheet(isPresented: $newTaskIsShowing, content: {
             NewTaskView(isVisible: self.$newTaskIsShowing, list: .inbox, project: nil, mainTask: nil)
                 .presentationDetents([.height(220)])
